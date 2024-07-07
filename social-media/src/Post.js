31dc13c46f58b431d9878ae93bce6e192733ebf4
Palinya Sengdalavong <76564437+Owenture01@users.noreply.github.com>
2024-07-07 13:37:30 +0800

    Post component

---
+++ social-media/src/Post.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+function Post({id, title}){
+    return(
+        <div className="Post"> 
+            <div className="Post_title">{title}</div>
+            <img 
+                className="Post_image" 
+                src={`https://source.unsplash.com/random?sig=${id}`}
+                alt = "Post"
+            />
+        </div>
+    )
+}
+
+Post.prototype = {
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired
+}
+
+export default Post;
