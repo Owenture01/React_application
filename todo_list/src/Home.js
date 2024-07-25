458fcba14c27b32c2f261e805b70313337fd47ba
Palinya Sengdalavong <76564437+Owenture01@users.noreply.github.com>
2024-07-25 13:12:39 +0800

    Add home page with button to TodoList

---
+++ todo_list/src/Home.js
@@ -0,0 +1,16 @@
+// Home.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to the Home Page</h1>
+      <Link to="/todolist">
+        <button>Go to Todo List</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
